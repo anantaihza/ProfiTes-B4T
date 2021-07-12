cb4c7c032ec0b34030144b110dff386e76a13d87
anantaihza <anantaihza21@gmail.com>
2021-07-12 14:58:20 +0700

    Login register Success

---
+++ app/Models/UsersModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id_user';
+    protected $returnType = 'object';
+    protected $useTimestamps = false;
+    protected $allowedFields = [
+        'id_user', 'username', 'email',
+        'password', 'nama_user', 'detail_alamat',
+        'no_telepon', 'no_fax'
+    ];
+}
