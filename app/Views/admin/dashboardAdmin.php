ae0ddc8d12b3c718b4acb76a5ac53e04260fd581
Masruri <alviyantoruri@gmail.com>
2021-08-19 15:10:28 +0700

    Revisi hampir akhir delet list perusahaan view

--- app/Views/admin/dashboardAdmin.php
+++ app/Views/admin/dashboardAdmin.php
@@ -10,6 +10,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="/assets/css/admin/dashboardAdmin.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <style>
+        .footer {
+            width: 100%;
+            height: 7vh;
+            background-color: #ffffff;
+        }
+
+        .btn-save {
+            background-color: #53b8bb;
+            border: none;
+            color: #ffffff;
+            border-radius: 0;
+        }
+
+        .btnlunas {
+            background-color: #0d6efd;
+            border: none;
+            color: #ffffff;
+            border-radius: 0;
+        }
+
+        .btn-save:hover {
+            background-color: #3c8083;
+            border-color: #656fc2;
+        }
+
+        .btnlunas:hover {
+            color: #ffffff;
+            background-color: #0d4eaf;
+        }
+    </style>
     <title>Dashboard</title>
 </head>
 
@@ -30,9 +61,6 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/listPaket">Paket</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/listPerusahaan">Perusahaan</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -147,43 +175,138 @@
             </div>
         </div>
 
-        <div class="card mt-4 mb-5">
-            <div class="card-body text-center">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-4">
-                            <div class="row my-auto">
-                                <div class="col-md-6 my-auto">
-                                    <p>Admin</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <a href="/listAdmin" class="btn btnpilih">Pilih</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="row my-auto">
-                                <div class="col-md-6 my-auto">
-                                    <p>Perusahaan</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <a href="/listPerusahaan" class="btn btnpilih">Pilih</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="row my-auto">
-                                <div class="col-md-6 my-auto">
-                                    <p>Paket</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <a href="/listPaket" class="btn btnpilih">Pilih</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div class="mt-2 mb-5 pb-5">
+
+            <div class="card container mt-4" style="background-color: white; padding:30px">
+                <h4>Daftar Perusahaan</h4>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover mt-4" style="text-align: center;">
+                        <thead style="background-color: #656fc2; color: white;  text-align: center;">
+                            <tr>
+                                <th class="align-middle">No</th>
+                                <th class="align-middle">Nama Perusahaan</th>
+                                <th class="align-middle">Username</th>
+                                <th class="align-middle">Email</th>
+                                <th class="align-middle">Alamat</th>
+                                <th class="align-middle">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1; ?>
+                            <?php foreach ($users as $user) : ?>
+                                <tr>
+                                    <th scope="row"><?= $i; ?></th>
+                                    <td><?= $user->nama_user; ?></td>
+                                    <td><?= $user->username; ?></td>
+                                    <td><?= $user->email; ?></td>
+                                    <td><?= $user->detail_alamat; ?></td>
+                                    <td>
+
+                                        <button type="button" class="btn btn-save px-3" data-bs-toggle="modal" data-bs-target="#perusahaan<?= $user->id_user; ?>">Detail</button>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="perusahaan<?= $user->id_user; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-lg">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel"><?= $user->nama_user; ?></h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="table-responsive">
+                                                            <table class="table table-hover table-bordered mt-4" style="text-align: center;">
+                                                                <thead style="background-color: #656fc2; color: white; text-align: center;">
+                                                                    <tr>
+                                                                        <th class="align-middle">No</th>
+                                                                        <th class="align-middle">Nama Lab</th>
+                                                                        <th class="align-middle">Paket</th>
+                                                                        <th class="align-middle">Biaya</th>
+                                                                        <th class="align-middle">Action</th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <?php $j = 1; ?>
+                                                                    <?php foreach ($administrasi as $adm) : ?>
+                                                                        <?php if ($adm->id_user === $user->id_user) : ?>
+                                                                            <tr>
+                                                                                <th scope="row"><?= $j; ?></th>
+                                                                                <td><?= $adm->nama_laboratorium; ?></td>
+                                                                                <td><?= $adm->nama_pengujian; ?></td>
+                                                                                <td>Rp. <?= $adm->biaya; ?></td>
+                                                                                <?php if ($adm->status_pembayaran === "Belum Lunas") : ?>
+                                                                                    <td>
+                                                                                        <form action="/Admin/buatLunas/<?= $adm->id_administrasi; ?>" method="post">
+                                                                                            <button type="submit" class="btn btnlunas px-3">Buat Lunas</button>
+                                                                                        </form>
+
+
+                                                                                    </td>
+                                                                                <?php elseif ($adm->status_pembayaran === "Sudah Lunas") : ?>
+                                                                                    <td>
+
+                                                                                        <!-- <button class="btn btn-secondary dropdown-toggle" style="background-color: transparent; border-color: transparent; color:#757575;" type="button" data-bs-toggle="dropdown">Menu</button> -->
+
+                                                                                        <!-- <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2"> -->
+                                                                                        <?php if (($adm->status_pengujian === "Belum") && ($adm->status_resi === "Belum")) : ?>
+                                                                                            <button class="btn btn-secondary dropdown-toggle" style="background-color: transparent; border-color: transparent; color:#757575;" type="button" data-bs-toggle="dropdown">Menu</button>
+                                                                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                                                                                                <li>
+                                                                                                    <!-- <form action="/Admin/inputPengiriman/" method="post">
+                                                                                                        <button type="submit" class="dropdown-item">Input Pengiriman</button>
+                                                                                                    </form> -->
+                                                                                                    <a class="dropdown-item" href="/inputPengiriman/<?= $adm->id_administrasi; ?>">Input Pengiriman</a>
+
+                                                                                                </li>
+                                                                                            </ul>
+                                                                                        <?php elseif (($adm->status_pengujian === "Belum") && ($adm->status_resi === "Sudah")) : ?>
+                                                                                            <?php foreach ($pengiriman as $kirim) : ?>
+                                                                                                <?php if ($kirim->id_administrasi == $adm->id_administrasi) : ?>
+                                                                                                    <?php if ($kirim->status_pengiriman === "Sedang Dikirim") : ?>
+                                                                                                        <p>Menunggu diterima oleh Penerima</p>
+                                                                                                    <?php elseif ($kirim->status_pengiriman === "Diterima") : ?>
+                                                                                                        <p>Sudah diterima, menunggu pengujian.</p>
+                                                                                                    <?php endif; ?>
+                                                                                                <?php endif; ?>
+                                                                                            <?php endforeach; ?>
+                                                                                        <?php elseif (($adm->status_pengujian === "Sudah") && ($adm->status_resi === "Sudah")) : ?>
+                                                                                            <button class="btn btn-secondary dropdown-toggle" style="background-color: transparent; border-color: transparent; color:#757575;" type="button" data-bs-toggle="dropdown">Menu</button>
+                                                                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                                                                                                <li>
+                                                                                                    <!-- <form action="/Admin/detailPengujian/" method="post">
+                                                                                                        <button class="dropdown-item">Lihat Pengujian</button>
+                                                                                                    </form> -->
+                                                                                                    <a class="dropdown-item" href="/detailPengujian/<?= $adm->id_administrasi; ?>">Detail Pengujian</a>
+                                                                                                </li>
+                                                                                            </ul>
+
+                                                                                        <?php endif; ?>
+                                                                                        <!-- </ul> -->
+
+                                                                                    </td>
+                                                                                <?php endif ?>
+                                                                            </tr>
+                                                                        <?php else : ?>
+                                                                            <?php $j = 0; ?>
+                                                                        <?php endif; ?>
+                                                                        <?php $j++; ?>
+                                                                    <?php endforeach; ?>
+
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <?php $i++; ?>
+                            <?php endforeach; ?>
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
+
         </div>
     </div>
 
